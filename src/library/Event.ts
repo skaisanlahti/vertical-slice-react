e4392f49e25d837a2d6ad75233ccd7234c805a39
Sami Kaisanlahti <skaisanlahti@mailfence.com>
2023-07-23 17:47:58 +0300

    Remove dispose methods

--- src/library/Event.ts
+++ src/library/Event.ts
@@ -1,29 +1,17 @@
 import { Subscriber, Unsubscriber } from "./State";
 
 export class Event<T> {
-  private disposed = false;
   private readonly subscribers = new Set<Subscriber<T>>();
 
   public publish(message: T): void {
-    if (this.disposed) {
-      throw new Error("Event has been disposed.");
-    }
     this.notify(message);
   }
 
   public subscribe(subscriber: Subscriber<T>): Unsubscriber {
-    if (this.disposed) {
-      throw new Error("Event has been disposed.");
-    }
     this.subscribers.add(subscriber);
     return () => {
       this.subscribers.delete(subscriber);
     };
-  }
-
-  public dispose(): void {
-    this.disposed = true;
-    this.subscribers.clear();
   }
 
   private notify(value: T): void {
--- src/library/State.ts
+++ src/library/State.ts
@@ -2,7 +2,6 @@ export type Subscriber<T> = (state: T) => void;
 export type Unsubscriber = () => void;
 export class State<T> {
   private state: T;
-  private disposed = false;
   private readonly subscribers = new Set<Subscriber<T>>();
 
   public constructor(initialState: T) {
@@ -10,34 +9,20 @@ export class State<T> {
   }
 
   public get(): T {
-    if (this.disposed) {
-      throw new Error("State has been disposed.");
-    }
     return this.state;
   }
 
   public set(nextState: T): void {
-    if (this.disposed) {
-      throw new Error("State has been disposed.");
-    }
     this.state = nextState;
     this.notify(nextState);
   }
 
   public subscribe(subscriber: Subscriber<T>): Unsubscriber {
-    if (this.disposed) {
-      throw new Error("State has been disposed.");
-    }
     subscriber(this.state);
     this.subscribers.add(subscriber);
     return () => {
       this.subscribers.delete(subscriber);
     };
-  }
-
-  public dispose(): void {
-    this.disposed = true;
-    this.subscribers.clear();
   }
 
   private notify(value: T): void {
